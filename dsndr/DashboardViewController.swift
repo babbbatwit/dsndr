3bb8e1c37242c0cbd18aacb1801f26f0c777c85e
Brenton <thebubfusion@gmail.com>
2021-01-19 08:52:17 -0500

    Altitude and distance

--- dsndr/DashboardViewController.swift
+++ dsndr/DashboardViewController.swift
@@ -6,14 +6,64 @@
 //
 
 import UIKit
+import CoreLocation
+
 
 class DashboardViewController: UIViewController {
+    @IBOutlet var altitudeLabel: UILabel!
+       
+    @IBOutlet var distanceLabel: UILabel!
+    
+    private let locationManager = CLLocationManager()
+       
+       override func viewDidLoad() {
+           super.viewDidLoad()
+           
+           locationManager.requestWhenInUseAuthorization()
+           locationManager.distanceFilter = kCLDistanceFilterNone
+           locationManager.desiredAccuracy = kCLLocationAccuracyBest
+           locationManager.startUpdatingLocation()
+           locationManager.delegate = self
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       }
+    
+    
+    func totalDistance(of locations: [CLLocation]) -> CLLocationDistance {
+        var distance: CLLocationDistance = 0.0
+        var previousLocation: CLLocation?
+        
+        locations.forEach { location in
+            if let previousLocation = previousLocation {
+                distance += location.distance(from: previousLocation)
+            }
+            previousLocation = location
+        }
+        
+        return distance
     }
-
-
 }
+
+extension DashboardViewController: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let lastLocation = locations.last {
+            let altitude = lastLocation.altitude
+            altitudeLabel.text = "Altitude: \(altitude)"
+        }
+        
+        var distance: CLLocationDistance = 0.0
+        var previousLocation: CLLocation?
+        
+        locations.forEach { location in
+            if let previousLocation = previousLocation {
+                distance += location.distance(from: previousLocation)
+            }
+            previousLocation = location
+        }
+        
+        distanceLabel.text = "Distance: \(distance)"
+    }
+}
+    
+    
 
