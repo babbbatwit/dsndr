cce5274615f2f782c4bcc8e2b9197c99242edf72
Brenton <thebubfusion@gmail.com>
2021-01-24 13:04:37 -0500

    comments, comments, comments

--- dsndr/DashboardViewController.swift
+++ dsndr/DashboardViewController.swift
@@ -17,52 +17,59 @@ class DashboardViewController: UIViewController {
     private let locationManager = LocationManager.shared
     private var seconds = 0
     private var timer: Timer?
-    private var distance = Measurement(value: 0, unit: UnitLength.meters)
+    private var distance = Measurement(value: 0, unit: UnitLength.miles)
     private var locationList: [CLLocation] = []
     
     var ride: Ride!
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
         seconds = 0
-        distance = Measurement(value: 0, unit: UnitLength.meters)
+        //sets intial disatnce to 0 and is using miles as units
+        distance = Measurement(value: 0, unit: UnitLength.miles)
+        //makes sure the location array is empty (deletes users previous ride)
         locationList.removeAll()
+        //updates values on screen
         updateDisplay()
+        //assigns the timner var a Timer with the refresh inverval of 1 second
         timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { _ in
           self.eachSecond()
         }
         startLocationUpdates()
-        
-        
-        configureView()
+
         
     }
-    
-    private func configureView() {
-    }
-    
+
+    //after timer updates adds one second to seconds
     func eachSecond() {
         seconds += 1
+        //used to update the screen with most current info
         updateDisplay()
     }
     
     private func updateDisplay() {
-        
+        //changes distanceLabel to show current distance
         distanceLabel.text = "Distance:  \(distance)"
+        //changes timeLabel to show current time
         timeLabel.text = "\(seconds)"
     }
     
     private func startLocationUpdates() {
+        //locationManger needs a delegate before it can run. Assigned to it's self
         locationManager.delegate = self
+        //locationManger has a nice activity type built in. It stops location services when the user is inside or not moving to save battery life
         locationManager.activityType = .fitness
+        //min distance in meters (default value) in order for the program to warrent a location update. (nice battery saver)
         locationManager.distanceFilter = 10
+        //calls locationManger and starts to track location using apples CoreLocation library
         locationManager.startUpdatingLocation()
     }
 
 }
 
 
-
+//This is a facy location updater. I did not write this myself, but I understand how it works. I was having some issues getting this to work on my own, so I turned to the saviors of any programmers career, the stack overflow people
 extension DashboardViewController: CLLocationManagerDelegate {
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
@@ -72,7 +79,7 @@ extension DashboardViewController: CLLocationManagerDelegate {
             
             if let lastLocation = locationList.last {
                 let delta = newLocation.distance(from: lastLocation)
-                distance = distance + Measurement(value: delta, unit: UnitLength.meters)
+                distance = distance + Measurement(value: delta, unit: UnitLength.miles)
             }
             
             locationList.append(newLocation)
