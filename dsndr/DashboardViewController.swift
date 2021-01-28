a8b8d54d23ee7e7dc4961ea72cc42bb14ba3568b
Bubfusion <thebubfusion@gmail.com>
2021-01-27 23:10:53 -0500

    Code clean up, defaultStates() function, invalidateAllTimers() function

--- dsndr/DashboardViewController.swift
+++ dsndr/DashboardViewController.swift
@@ -22,17 +22,17 @@ class DashboardViewController: UIViewController {
     
     //location variables and variables that will be displayed
     private let locationManager = LocationManager.shared
-    private var seconds = 0
-    private var distance = Measurement(value: 0, unit: UnitLength.meters)
-    private var altitude = Measurement(value: 0, unit: UnitLength.meters)
+    private var seconds: Int = 0
+    private var distance: Measurement = Measurement(value: 0, unit: UnitLength.meters)
+    private var altitude: Measurement = Measurement(value: 0, unit: UnitLength.meters)
     private var locationList: [CLLocation] = []
-    private var laps = 0
+    private var laps: Int = 0
 
     //behind the scences doubles
-    private var currentAltitude = 0.0
-    private var previousAltitude = 0.0
-    private var currentDistance = 0.0
-    private var previousDistance = 0.0
+    private var currentAltitude: Double = 0.0
+    private var previousAltitude: Double = 0.0
+    private var currentDistance: Double = 0.0
+    private var previousDistance: Double = 0.0
     
     //timers used for checking various things
     private var hasUpdatedTimer: Timer?
@@ -41,14 +41,14 @@ class DashboardViewController: UIViewController {
     private var duration: Timer?
     private var resumeTimer: Timer?
     //booleans and their default states on launch
-    private var wasJustStaretd = true
-    private var hasUpdated = false
-    private var isStopped = false
-    private var isAscending = false
-    private var timeToCheck = false
-    private var wasPaused = false
-    private var isPaused = false
-    private var userPaused = false
+    private var wasJustStaretd: Bool = true
+    private var hasUpdated: Bool = false
+    private var isStopped: Bool = false
+    private var isAscending: Bool = false
+    private var timeToCheck: Bool = false
+    private var wasPaused: Bool = false
+    private var isPaused: Bool = false
+    private var userPaused: Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -70,27 +70,12 @@ class DashboardViewController: UIViewController {
     //fucntion used when the stop button is pressed. Hides and reveals specific buttons and ensures that all variables return to their default state
     @IBAction func stopPressed(_ sender: Any) {
         locationManager.stopUpdatingLocation()
-        stopTimer()
-        seconds = 0
-        laps = 0
-        distance = Measurement(value: 0, unit: UnitLength.meters)
-        altitude = Measurement(value: 0, unit: UnitLength.meters)
+        invalidateAllTimers()
+        defaultStates()
         startButton.isHidden = false
         stopButton.isHidden = true
         pauseButton.isHidden = true
         resumeButton.isHidden = true
-        pauseCheckerTimer?.invalidate()
-        hasUpdatedTimer?.invalidate()
-        rideStartTimer?.invalidate()
-        duration?.invalidate()
-        resumeTimer?.invalidate()
-        hasUpdated = false
-        isStopped = false
-        isAscending = false
-        timeToCheck = false
-        wasPaused = false
-        isPaused = false
-        userPaused = false
         updateDisplay()
     }
     //fucntion used when the pause button is pressed. Hides and reveals specific buttons
@@ -103,6 +88,40 @@ class DashboardViewController: UIViewController {
         resumeTracking()
         userPaused = false
     }
+    
+    //resets all variables to their default states
+    func defaultStates(){
+        seconds = 0
+        distance = Measurement(value: 0, unit: UnitLength.meters)
+        altitude = Measurement(value: 0, unit: UnitLength.meters)
+        locationList = []
+        laps = 0
+
+        //behind the scences doubles
+        currentAltitude = 0.0
+        previousAltitude = 0.0
+        currentDistance = 0.0
+        previousDistance = 0.0
+        
+
+        //booleans and their default states on launch
+        wasJustStaretd = true
+        hasUpdated = false
+        isStopped = false
+        isAscending = false
+        timeToCheck = false
+        wasPaused = false
+        isPaused = false
+        userPaused = false
+    }
+    func invalidateAllTimers() {
+        pauseCheckerTimer?.invalidate()
+        hasUpdatedTimer?.invalidate()
+        rideStartTimer?.invalidate()
+        duration?.invalidate()
+        resumeTimer?.invalidate()
+    }
+    
     //Fucntion used when app needs to pause tracking. Specifically does not stop location services, but stops timer. Turns on variables that are then used later to pause to collection of total distance
     func pauseTracking(){
         stopTimer()
@@ -114,6 +133,7 @@ class DashboardViewController: UIViewController {
         isPaused = true
         updateDisplay()
     }
+    
     //Resumes tracking the users distance and adding that to the total amount of distance. Turns need booleans to on or off depending on what they are asking
     func resumeTracking(){
         startTimer()
@@ -125,6 +145,7 @@ class DashboardViewController: UIViewController {
         isStopped = false
         updateDisplay()
     }
+    
     //This makes sure to wipe any current data. Starts a one time timer that acts as a buffer so the app doesn't automatically pause tracking when the app has just started
     func startRide() {
         //starts a repeating timer that calls liftChecker() and movingChecker()
@@ -142,11 +163,13 @@ class DashboardViewController: UIViewController {
         startButton.isHidden = true
         stopButton.isHidden = false
     }
+    
     //The duration timer calls this every second. Just adds one to seconds which is then displayed in the format of 00:00:00 to the user
     func eachSecond() {
         seconds += 1
         updateDisplay()
     }
+    
     //This starts the timer that tracks total duration. Every 1 seconds the timer updates which calls the eachSecond() function which adds 1 to the seconds variable
     func startTimer(){
         stopTimer()
@@ -154,10 +177,12 @@ class DashboardViewController: UIViewController {
             self.eachSecond()
         }
     }
+    
     //invailidates the duration timer. It stops it from ticking, but does not wipe the seconds variable
     func stopTimer() {
         duration?.invalidate()
     }
+    
     //when ever something changes in the app this function is called. It is specifcally for displaying the useful collected information to the user. Calls over to the Formatting.swift file to format the text properly for viewing
     private func updateDisplay() {
         let formattedDistance = Formatting.distance(distance)
@@ -169,6 +194,7 @@ class DashboardViewController: UIViewController {
         timeLabel.text = formattedTime
         lapsLabel.text = "Laps: \(laps)"
     }
+    
     //Sets locactionMangagers prefrences. This provides accuracy for the app. Specifically for bug testing right now
     private func startLocationUpdates() {
         locationManager.delegate = self
@@ -179,6 +205,7 @@ class DashboardViewController: UIViewController {
         locationManager.pausesLocationUpdatesAutomatically = true
         locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
     }
+    
     //liftChecker is the function that checks if the user is gaining alitude instead of losing altitude. If the user is ascending then it calls pauseTracking(), sets laps to +1, and turns specfific variables to what they need to be
     func liftChecker() {
         if previousAltitude - currentAltitude > 5 && currentAltitude != 0 && previousAltitude != 0 && wasJustStaretd == false  {
@@ -198,6 +225,7 @@ class DashboardViewController: UIViewController {
             }
         }
     }
+    
     //checks if the locationManager() function has turned hasUpdated to false, which means the user hasn't moved with in a 10 second period. Pauses tracking.
     func movingChecker() {
         if hasUpdated == false && wasJustStaretd == false {
@@ -215,6 +243,7 @@ class DashboardViewController: UIViewController {
         }
     }
     
+    //Grace period function for when tracking resumes. Prevents auto pause for 15 seconds
     func startResumeTimer(){
         wasJustStaretd = true
         rideStartTimer?.invalidate()
