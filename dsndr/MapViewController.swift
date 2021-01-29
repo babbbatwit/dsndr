ad71acb92e7c7cd9abebb22444943d901aadf0f6
Bubfusion <thebubfusion@gmail.com>
2021-01-29 11:11:19 -0500

    Map zooms to user's location

--- dsndr/MapViewController.swift
+++ dsndr/MapViewController.swift
@@ -8,13 +8,30 @@
 import UIKit
 import MapKit
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, CLLocationManagerDelegate {
     @IBOutlet var mapView: MKMapView!
     //all this does is show the user's pin on a map
     override func viewDidLoad() {
         super.viewDidLoad()
+        let noLocation = CLLocationCoordinate2D()
+        let viewRegion = MKCoordinateRegion(center: noLocation, latitudinalMeters: 200, longitudinalMeters: 200)
+        mapView.setRegion(viewRegion, animated: false)
         mapView.showsUserLocation = true
-    }
-    
+        
+           let locationManager = CLLocationManager()
+           locationManager.delegate = self
+           locationManager.desiredAccuracy = kCLLocationAccuracyBest
 
+           // Check for Location Services
+           if (CLLocationManager.locationServicesEnabled()) {
+               locationManager.requestAlwaysAuthorization()
+               locationManager.requestWhenInUseAuthorization()
+           }
+
+           //Zoom to user location
+           if let userLocation = locationManager.location?.coordinate {
+            let viewRegion = MKCoordinateRegion(center: userLocation, latitudinalMeters: 200, longitudinalMeters: 200)
+               mapView.setRegion(viewRegion, animated: false)
+           }
+    }
 }
